b33464e877dedda1439d67a069157f1e2034eb19
valentinaiori <valentinaiori@live.it>
2022-11-06 14:44:14 +0100

    Add exercise complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
-@dd($movies)
+@extends('layout/layout')
+
+@section('main')
+    <h1>My DB Movies</h1>
+    <div class="card-container">
+
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h5>Titolo:</h5>
+                <h2>{{ $movie['title'] }}</h2>
+                <h5>Titolo Originale:</h5>
+                <h3>{{ $movie['original_title'] }}</h3>
+                <h5>Nazionalita':</h5>
+                <p>{{ $movie['nationality'] }}</p>
+                <h5>Data Uscita:</h5>
+                <p>{{ $movie['date'] }}</p>
+                <h5>Voto:</h5>
+                <span>{{ $movie['vote'] }}</span>
+            </div>
+        @endforeach
+    </div>
+@endsection
+
