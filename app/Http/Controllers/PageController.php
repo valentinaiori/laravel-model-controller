85de002c51c2c8360d9e512d1ee802978d471789
valentinaiori <valentinaiori@live.it>
2022-11-06 14:23:34 +0100

    PageController: get all movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     //
+
+    public function index(){
+        $movies = Movie::all();
+        return 'ciao';
+    }
 }
