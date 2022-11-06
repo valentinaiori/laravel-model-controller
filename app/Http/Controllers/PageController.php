ee518ff5053007034ebd8a08e1ff6e7871799d35
valentinaiori <valentinaiori@live.it>
2022-11-06 14:26:12 +0100

    Add view home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,6 +11,6 @@ class PageController extends Controller
 
     public function index(){
         $movies = Movie::all();
-        return 'ciao';
+        return view('home', compact('movies'));
     }
 }
